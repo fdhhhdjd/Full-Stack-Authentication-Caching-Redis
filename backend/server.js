691089549715eb828a939d90f01c44cb6367280b
tai <nguyentientai10@gmail.com>
2022-08-30 09:24:27 +0700

    setup success

---
+++ backend/server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const responseTime = require("response-time");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const Redis = require("ioredis");
+let RedisStore = require("connect-redis")(session);
+const compression = require("compression");
+let redisClient = new Redis();
+const app = express();
+require("./configs/db");
+require("./configs/redis");
+app.enable("trust proxy");
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: process.env.KEY_SESSION,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      maxAge: 5 * 60 * 1000,
+    },
+  })
+);
+app.use(
+  compression({
+    level: 6,
+    threshold: 100 * 1000,
+    filter: (req, res) => {
+      if (req.headers["x-no-compression"]) {
+        return false;
+      }
+      return compression.filter(req, res);
+    },
+  })
+);
+app.use(responseTime());
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  express.json({
+    verify: (req, res, buffer) => (req["rawBody"] = buffer),
+  })
+);
+app.get("/", (req, res) => {
+  const healthcheck = {
+    uptime: process.uptime(),
+    message: "Tài Đẹp trai ",
+    timestamp: Date.now(),
+  };
+  return res.send(healthcheck);
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`server is listening on port:http://localhost:${PORT}`)
+);
